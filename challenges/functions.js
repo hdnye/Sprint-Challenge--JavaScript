da89914a8976f57827f6a0d2bbc3c9b78a53f048
Heather Nye <heatherdnye@gmail.com>
2019-09-16 19:40:05 -0500

    Sprint-Challenge--javascript callbacks

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,18 +7,39 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(a, b, callback) {
+  return callback(a, b);
+}
+
+// 
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(num1, num2) {
+  return num1 + num2;
+}
 
+console.log(consume(2, 3, add));
+
+function multiplyNums(num1, num2) {
+    return num1 * num2;
+  };
+  
+  console.log(consume(2, 3, multiplyNums));
+
+  function greeting(_first_name, _last_name) {
+    return `Hello ${_first_name} ${_last_name}, nice to meet you!`;
+  };
+
+console.log(consume('Heather', 'Nye', greeting));
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiplyNums)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
